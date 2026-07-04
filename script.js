a84ad1d9722056591047098cc8c87edfcae190b7
Arjya Kumar Paul <arjya2004@gmail.com>
2026-07-04 23:56:13 +0530

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const yearEl = document.getElementById("currentYear");
+
+    if (yearEl) {
+        yearEl.textContent = new Date().getFullYear();
+    }
+});
